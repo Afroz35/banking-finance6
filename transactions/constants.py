e8149e2503e58380c8e4e03e5ddc1a12c9190502
Afroz35 <110450695+Afroz35@users.noreply.github.com>
2024-01-10 14:54:14 +0530

    pythonn

---
+++ transactions/constants.py
@@ -0,0 +1,10 @@
+DEPOSIT = 1
+WITHDRAWAL = 2
+INTEREST = 3
+LOAN=0
+TRANSACTION_TYPE_CHOICES = (
+    (DEPOSIT, 'Deposit'),
+    (WITHDRAWAL, 'Withdrawal'),
+    (INTEREST, 'Interest'),
+    (LOAN,'loan'),
+)
